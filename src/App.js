8ce56de47beaa196f6dda104a2cbea83f4a608a8
LaurentArcos <laurent.arcos@gmail.com>
2023-04-20 18:33:07 +0200

    axios request ok

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
-import React from 'react'
+import axios from 'axios';
+import {React, useState, useEffect} from 'react'
 
 const App = () => {
+
+  const [profileData, setProfileData ] =useState([]);
+
+
+  useEffect (() =>{
+  axios
+    .get('https://eu.api.blizzard.com/profile/user/wow?namespace=profile-eu&locale=fr_FR&access_token=EUCK6Zj93oPd3aTZa98RKneHRxIbgWtrf2')
+    .then((response) => {
+      setProfileData(response.data);
+      console.log(response.data);
+    })
+    .catch((error) => {
+      console.error(error);
+    })
+  }, [])
+
+
   return (
-    <div>Bonjour</div>
+    <div>{profileData.wow_accounts[0].characters[0].name}</div>
   )
 }
 
