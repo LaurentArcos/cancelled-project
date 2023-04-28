0df18f15e1a29fe42bc6ad3908ec487ede86f2a9
LaurentArcos <laurent.arcos@gmail.com>
2023-04-28 17:00:39 +0200

    axios request for character profile pic

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import axios from 'axios';
 import {React, useState, useEffect} from 'react'
+import './app.scss';
 
 const App = () => {
 
   const [profileName, setProfileName ] =useState([]);
+  const [profilePicture, setProfilePicture] =useState([]);
 
 
   useEffect (() =>{
   axios
-    .get('https://eu.api.blizzard.com/profile/user/wow?namespace=profile-eu&locale=fr_FR&access_token=EUCK6Zj93oPd3aTZa98RKneHRxIbgWtrf2')
+    .get('https://eu.api.blizzard.com/profile/wow/character/uldaman/thryndil/character-media?namespace=profile-eu&locale=fr_FR&access_token=EU6fwhiW2HklpTVG89ipSmpDnHjwWDyAyp')
     .then((response) => {
-      setProfileName(response.data.wow_accounts[0].characters[0].name);
+      console.log(response);
+      setProfileName(response.data.character.name);
+      setProfilePicture(response.data.assets[2].value)
     })
     .catch((error) => {
       console.error(error);
@@ -19,7 +23,9 @@ const App = () => {
 
 
   return (
-    <div>{profileName}</div>
+    <div className='background' style={{ backgroundImage: `url(${profilePicture})` }}>
+      <div className='name'>{profileName}</div>
+    </div>
   )
 }
 
