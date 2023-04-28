493152c740791e37f78c9d85e3652b05f13fae51
LaurentArcos <laurent.arcos@gmail.com>
2023-04-28 17:04:03 +0200

    comments to indicate the url location on Blizzard API website

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ const App = () => {
 
   useEffect (() =>{
   axios
+    // request from Profile APIs --> Character Media API --> Character Media Summary  
     .get('https://eu.api.blizzard.com/profile/wow/character/uldaman/thryndil/character-media?namespace=profile-eu&locale=fr_FR&access_token=EU6fwhiW2HklpTVG89ipSmpDnHjwWDyAyp')
     .then((response) => {
       console.log(response);
